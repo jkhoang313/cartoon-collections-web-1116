81dde03c5ee275c3b4c57319942ed70d22862852
Jackson Hoang <jkhoang313@gmail.com>
2016-11-21 16:17:25 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,6 @@ def summon_captain_planet(array)# code an argument here
   array.collect do |element|
     element.capitalize << "!"
   end
-  
 end
 
 def long_planeteer_calls(array)# code an argument here
